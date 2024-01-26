9f539002db09f072b14c563b6df28f1ee29b897b
Prachi Mishra <143651644+misprachi023@users.noreply.github.com>
2024-01-26 19:31:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app=express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:'http://127.0.0.1:5173',
+    origin:'https://fullstack-frontend-rho.vercel.app/',
     credentials:true
 }));
 app.options("*", cors());
